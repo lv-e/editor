8f6f9e36b6b521e2b6346a5263403805882792e3
Lino <lgustavo@enjoei.com.br>
2020-02-25 20:23:51 -0300

    pre-electron

--- editor-app/renderer/simulator/src/app/app.component.ts
+++ editor-app/renderer/simulator/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h2>simulator</h2>`,
+  template: `<div style="text-align:center" class="content"><h1>{{title}}</h1></div>`,
   styles: []
 })
 export class AppComponent {
