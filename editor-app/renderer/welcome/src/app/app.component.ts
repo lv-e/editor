f76c46af73d35d72cfba9e50a4d892441696d297
Lino <lgustavo@enjoei.com.br>
2020-02-25 19:37:27 -0300

    adding angular

---
+++ editor-app/renderer/welcome/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div style="text-align:center" class="content"><h1>{{title}}</h1></div>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'welcome';
+}
