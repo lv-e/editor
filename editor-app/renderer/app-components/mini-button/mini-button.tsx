bcd2e9eb41dbc855d66e9ecf6c531db8dc0d06ba
lino <luizgustavolino@gmail.com>
2020-07-04 12:01:15 -0300

    adding IPC to welcome window

--- editor-app/renderer/app-components/mini-button/mini-button.tsx
+++ editor-app/renderer/app-components/mini-button/mini-button.tsx
@@ -4,11 +4,12 @@ import "./mini-button.less"
 
 interface MiniButtonProps {
     icon:("close" | "maximize" | "minimize")
+    onclick:(event:React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
 }
 
 export const MiniButton: React.SFC<MiniButtonProps> = (props) => {
     return (
-        <button className="colorize minibutton">
+        <button className="colorize minibutton" onClick={props.onclick}>
             <img src="1px.png" className={`minibutton icon icon-${props.icon}`}/>
         </button>
     )
--- editor-app/renderer/welcome/src/components/header/header.tsx
+++ editor-app/renderer/welcome/src/components/header/header.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import * as Electron from "electron";
+
 import { MiniButton } from "../../../../app-components/mini-button/mini-button";
 import "./header.less"
 
@@ -6,9 +8,14 @@ declare function require(path: string);
 const logo = require('../../../../app-assets/images/bg/header-logo-bg.png');
 
 export function Header (props) {
+
+    function handleClose(event:React.MouseEvent<HTMLButtonElement, MouseEvent>){
+        Electron.ipcRenderer.send("welcome:close")
+    }
+
     return <>
     <section id="welcome-header">
-        <MiniButton icon="close"/>
+        <MiniButton icon="close" onclick={handleClose}/>
         <div className="logo">
             <span className="stripe"/>
             <img src={String(logo)}/>
