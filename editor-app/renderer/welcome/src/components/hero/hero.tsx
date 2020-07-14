8ae5964533bd0c030392961ed97acf350053743b
lino <luizgustavolino@gmail.com>
2020-07-13 23:38:53 -0300

    updating rules for hero loader

--- editor-app/renderer/welcome/src/components/hero/hero.tsx
+++ editor-app/renderer/welcome/src/components/hero/hero.tsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import "./hero.less"
 
 export function Hero (props) {
+    const artworkCount = 3
+    const selected = 1 + (new Date().getDay()) % artworkCount
     return  <section id="hero">
-                <img className="logo"/>
+                <img className={`logo type${selected}`}/>
             </section>
 }
