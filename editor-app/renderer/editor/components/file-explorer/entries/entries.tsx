143ddb6ed208ab1928477aeb4ada22b90e34afff
lino <luizgustavolino@gmail.com>
2020-07-30 00:55:11 -0300

    navigating into folders

--- editor-app/renderer/editor/components/file-explorer/entries/entries.tsx
+++ editor-app/renderer/editor/components/file-explorer/entries/entries.tsx
@@ -3,16 +3,47 @@ import * as lv from "@lv-game-editor/lv-cli"
 
 import "./entries.less"
 
-export type DirKind = ("inner" | "shared" | "scene")
+export type DirKind = ("folder" | "shared" | "scene")
+
 export interface DirEntryProps {
     dir:lv.dirMap
     kind:DirKind
+    level:number
+}
+
+interface FileEntryProps {
+    file:lv.fileMap
+    level:number
+}
+
+export function DirList ({dirs, kind, level}:{
+    dirs:(lv.dirMap[]|null),
+    kind:DirKind
+    level:number
+}){
+    return <>
+        { dirs != null 
+            && dirs.map( (dir, i) => <DirEntry kind={kind} dir={dir} key={i} level={level}/> )
+        }
+    </>
+}
+
+export function FileList ({files, level}:{
+    files:(lv.fileMap[]|null),
+    level:number
+}){
+    return <>
+        { files != null 
+            && files.map( (dir, i) => <FileEntry file={dir} key={i} level={level}/> )
+        }
+    </>
 }
 
 export const DirEntry: React.SFC<DirEntryProps> = (props) => {
 
     let display_name:string
     let display_extension:string
+    let [openned, setOpenned] = React.useState(false)
 
     switch (props.kind) {
         case "shared":
@@ -23,31 +54,47 @@ export const DirEntry: React.SFC<DirEntryProps> = (props) => {
             display_name = props.dir.name.replace("scene_", "")
             display_extension = "scene"
             break 
+        case "folder":
+            display_name = props.dir.name
+            display_extension = "folder"
+            break 
     }  
 
-    return  <li className="dir-entry">
-                <button className="colorize" onClick={ e => console.log(e) }>
-                    <i className={`ico-${props.kind}`}/>
-                    <span className="title">{display_name}</span>
-                    <span className="extension">{display_extension}</span>
-                </button>
-            </li>
-}
+    const stateIcon = openned ? "minus" : "plus"
 
-interface FileEntryProps {
-    file:lv.fileMap
+    return  <>
+        <li className="dir-entry">
+            <button className="colorize" onClick={ e => setOpenned(!openned) }>
+                <i className={`ico-${stateIcon}`} style={{marginLeft: `${props.level * 14}px`}}/>
+                <i className={`ico-${props.kind}`}/>
+                <span className="title">{display_name}</span>
+                <span className="extension">{display_extension}</span>
+            </button>
+        </li>
+        { props.dir.directories != null && openned
+            && <DirList dirs={props.dir.directories} kind="folder" level={props.level + 1}/>
+        }
+        { props.dir.files != null && openned
+            && <FileList files={props.dir.files} level={props.level + 1}/>
+        }
+    </>
 }
 
 export const FileEntry: React.SFC<FileEntryProps> = (props) => {
 
     let ico:string = "file"
+
     switch (props.file.extension){
-        case ".lvproject": ico = "ico-cartridge"; break 
+        case ".lvproject": ico = "cartridge"; break;
+        case ".lvcode": ico = "file-lvcode"; break;
+        case ".lvsprite": ico = "file-lvsprite"; break;
+        default: ico = "file-any"; break;
     }
 
     return  <li className="file-entry">
                 <button className="colorize" onClick={ e => console.log(e) }>
-                    <i className="ico-cartridge"/>
+                    <i className="ico-align" style={{marginLeft: `${props.level * 14}px`}}/>
+                    <i className={`ico-${ico}`}/>
                     <span className="title">{props.file.name}</span>
                     <span className="extension">{props.file.extension}</span>
                 </button>
--- editor-app/renderer/editor/components/file-explorer/files.tsx
+++ editor-app/renderer/editor/components/file-explorer/files.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as lv from "@lv-game-editor/lv-cli"
 import { ipc } from "[comps]/electron/ipcRenderer"
-import {FileEntry, DirEntry, DirEntryProps, DirKind} from "./entries/entries"
+import {FileEntry, DirEntry, DirEntryProps, DirKind, DirList} from "./entries/entries"
 
 import "./file-explorer.less"
 import { useState, useEffect } from "react";
@@ -19,22 +19,14 @@ export function Files (props) {
     return  <section id="explorer-files">
                 <ul>
                     { rootFolders != null 
-                        && <FileEntry file={rootFolders.project_file}/>
+                        && <FileEntry file={rootFolders.project_file} level={0}/>
                     }
                     { rootFolders != null && rootFolders.shared != null 
-                        && <DirList dirs={rootFolders.shared} kind="shared"/>
+                        && <DirList dirs={rootFolders.shared} kind="shared" level={0}/>
                     }
                     { rootFolders != null && rootFolders.scenes != null 
-                        && <DirList dirs={rootFolders.scenes} kind="scene"/>
+                        && <DirList dirs={rootFolders.scenes} kind="scene" level={0}/>
                     }
                 </ul>
             </section>
-}
-
-function DirList ({dirs, kind}:{ dirs:(lv.dirMap[]|null), kind:DirKind}){
-    return <>
-        { dirs != null 
-            && dirs.map( (dir, i) => <DirEntry kind={kind} dir={dir} key={i}/> )
-        }
-    </>
 }
